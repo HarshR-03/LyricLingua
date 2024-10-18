c777314d675608e83510f8c862d73d6645e752e4
HarshR-03 <152909344+HarshR-03@users.noreply.github.com>
2024-10-18 17:53:13 +0530

    Update scrape3.py

--- Backend/getlyrics/scrape3.py
+++ Backend/getlyrics/scrape3.py
@@ -22,8 +22,9 @@ async def get_query_from_chat(data):
             role="system", content='''I will give you a video title and description in romaji.
                                     You only extract the song title and artist name from them and, if applicable, 
                                     convert the romaji to japanese kana/kanji and return the answer in the format:
-                                    "j-lyric <songname in hiragana/katakana/kanji/english> <artistname in hiragana/katakana/kanji/english>."
-                                    Return only the answer DON'T return any additional notes or metadata '''
+                                    "j-lyric uta-5 lyricalnonsense <songname in hiragana/katakana/kanji/english> <artistname in hiragana/katakana/kanji/english>."
+                                    If possible stick to hiragana/katakana/kanji if the song title and artist name are in japanese
+                                    Return ONLY IN THE FORMAT SPECIFIED, DON'T return any additional notes or metadata '''
         ),
         ChatMessage(role="user", content=f'''{data}'''),
     ]
