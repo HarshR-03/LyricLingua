0a7e50c989649f3bc58ea6665b73f9a8387835fb
HarshR-03 <btech10784.22@gmail.com>
2026-09-22 20:25:57 +0530

    fix: fixing vercel.ts

--- Frontend/Frontend/vercel.ts
+++ Frontend/Frontend/vercel.ts
@@ -1,4 +1,4 @@
-import { routes, type VercelConfig } from '@vercel/config/v1';
+import { routes, type VercelConfig, deploymentEnv } from '@vercel/config/v1';
 
 const BACKEND_URL = process.env.BACKEND_URL;
 
@@ -6,7 +6,7 @@ export const config: VercelConfig = {
   rewrites: [
     routes.rewrite(
       '/backend-api/:path*',
-      `${BACKEND_URL}/:path*`
+      deploymentEnv('BACKEND_URL') + "/:path*"
     ),
   ],
 };
